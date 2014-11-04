babf53ff923e69a2ecf95ae77d2970b23f081907
tdpetrou <petrou.theodore@gmail.com>
2014-11-03 17:05:18 -0800

    building first model

---
+++ build_model.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+import re
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestRegressor
