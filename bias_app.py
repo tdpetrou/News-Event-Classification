c538d994ddf6124045fe3b9f466964253dce3cab
tdpetrou <petrou.theodore@gmail.com>
2014-11-05 08:38:05 -0800

    added sentiment scores

--- bias_app.py
+++ bias_app.py
@@ -74,15 +74,27 @@ def google_yahoo_news():
     divs = [div for div in soup.findAll('div', 'esc-lead-article-title-wrapper')]
     links = []
     bias_scores = []
+    titles = []
+    sentiments = []
+    sentiment_scores = []
+    sources = [source.text for source in soup.findAll('span', {'class': 'al-attribution-source'})]
     for div in divs:
         link = div.find_all('a', href=True)
         links.append(link[0]['href'])
+        titles.append(link[0].findAll('span')[0].text)
     for link in links:
         response = alchemyapi.text('url', link)
         text = str(re.sub('[^\w\s]+', '', response['text']))
         text = str(re.sub('\n+', '', text)) 
         bias_scores.append(model.predict(vec.transform([text]).toarray())[0])
-    return render_template('google_news_temp.html', data = zip(links, bias_scores ))
+
+        #get sentiment
+        response = alchemyapi.sentiment('url', link)
+        if response.has_key('docSentiment'):
+            print response['docSentiment'].get('type', 0) , response['docSentiment'].get('score', 0)
+            sentiments.append(response['docSentiment']['type'])
+            sentiment_scores.append(response['docSentiment'].get('score', 0))
+    return render_template('google_news_temp.html', data = zip(titles, links, sources, bias_scores, sentiments, sentiment_scores))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7070, debug=True)
